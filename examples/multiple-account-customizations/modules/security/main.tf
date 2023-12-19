0b5263e4c681367e51c3859f30cbcecef087e459
padhu86 <37106046+padhu86@users.noreply.github.com>
2023-12-19 13:33:07 +0530

    Delete examples directory

--- examples/multiple-account-customizations/modules/security/main.tf
+++
@@ -1,8 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-resource "aws_ssm_parameter" "example_parameter_security" {
-  name  = "/aft/example/parameter_security"
-  type  = "String"
-  value = "Security customizations"
-}
